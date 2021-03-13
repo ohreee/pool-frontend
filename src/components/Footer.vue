d3f5c3c0c864669e05b2cd413936039949224dd8
mechaadi <as808780@gmail.com>
2021-03-13 19:14:02 +0530

    fixed footer on mobile view

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ohr-footer-wrapper">
     <div class="ohr-footer-desktop">
+      <img alt="logo" class="ohr-footer-desktop-logo" src="@/assets/logo-desktop.svg"/>
       <div class="ohr-row ohr-v-center ohr-h-100">
         <div class="ohr-col-2 ohr-v-center ohr-row ohr-copyright">
           © 2021 Ohreee Ltd.
@@ -9,17 +10,17 @@
           <ul>
             <li>About</li>
             <li>Docs</li>
-            <li>Donate</li>
-            <li>Language</li>
+            <li>Donate <img src="@/assets/icons/heart.svg" /></li>
+            <li>Language <img src="@/assets/icons/united-kingdom.svg" /></li>
           </ul>
         </div>
         <div class="ohr-col-4 ohr-row ohr-social-links ohr-right ohr-v-center">
-            <img src="@/assets/icons/medium.svg"/>
-            <img src="@/assets/icons/twitter.svg"/>
-            <img src="@/assets/icons/instagram.svg"/>
-            <img src="@/assets/icons/discord.svg"/>
-            <img src="@/assets/icons/telegram.svg"/>
-            <img src="@/assets/icons/github.svg"/>
+          <img src="@/assets/icons/medium.svg" />
+          <img src="@/assets/icons/twitter.svg" />
+          <img src="@/assets/icons/instagram.svg" />
+          <img src="@/assets/icons/discord.svg" />
+          <img src="@/assets/icons/telegram.svg" />
+          <img src="@/assets/icons/github.svg" />
         </div>
       </div>
     </div>
@@ -31,6 +32,8 @@ export default {};
 </script>
 
 <style lang="sass" scoped>
+.ohr-footer-desktop-logo
+  display: none
 .ohr-footer-wrapper
     background-color: #ffffff
     width: 100%
@@ -46,6 +49,10 @@ export default {};
     .ohr-copyright
         color: #252E65
         font-size: 21px
+    ul
+      width: 100%
+      display: flex
+      justify-content: flex-end
     li
         display: inline
         font-size: 21px
@@ -60,15 +67,98 @@ export default {};
     .ohr-footer-desktop, .ohr-footer-wrapper
         height: 100px !important
     .ohr-copyright
-        flex: 50% !important
-        max-width: 50% !important
+        flex: 30% !important
+        max-width: 30% !important
         justify-content: flex-start
     .ohr-menu-links
-        flex: 50% !important
-        max-width: 50% !important
+        flex: 70% !important
+        max-width: 70% !important
         justify-content: flex-end
     .ohr-social-links
         flex: 100% !important
         max-width: 100% !important
         justify-content: center !important
+
+
+@media only screen and (max-width: 786px)
+  .ohr-footer-desktop, .ohr-footer-wrapper
+    height: 80px !important
+    padding: 4px !important
+
+  .ohr-copyright
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: center
+  .ohr-menu-links
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: center
+  .ohr-social-links
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: center !important
+
+  ul
+    justify-content: center !important
+
+@media only screen and (max-width: 576px)
+  .ohr-footer-desktop-logo
+    display: block !important
+    width: 130px
+    padding: 8px
+    margin-bottom: 16px 
+  .ohr-footer-desktop, .ohr-footer-wrapper
+    height: 80px !important
+    padding-bottom: 8px
+    padding: 8px !important
+  .ohr-copyright
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: flex-start
+    font-size: 14px !important
+    color: #707070 !important
+    order: 3 !important
+    height: 60px !important
+
+  .ohr-menu-links
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: flex-start
+    order: 1 !important
+    height: 140px
+  .ohr-social-links
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: center !important
+    order: 2 !important
+    height: 60px !important
+    img
+      height: 30px
+      width: 30px
+      margin: 8px !important
+  ul
+    display: block !important
+  li
+    display: block !important
+    margin: 14px 0 !important
+    color: #707070 !important
+    font-size: 14px !important
+
+@media only screen and (max-width: 450px)
+  .ohr-footer-desktop, .ohr-footer-wrapper
+    height: 80px !important
+    padding-bottom: 8px
+
+  .ohr-social-links
+    flex: 100% !important
+    max-width: 100% !important
+    justify-content: flex-start !important
+    order: 2 !important
+
+@media only screen and (max-width: 358px)
+    .ohr-social-links
+      img
+        height: 20px
+        width: 20px
+        margin: 8px !important
 </style>
